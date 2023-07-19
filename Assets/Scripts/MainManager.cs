bfeb3df22f69439c533c4b27bd5560082ef24649
Yurkie <98610776+YurkiewB@users.noreply.github.com>
2023-07-19 11:15:01 -0700

    Data Persistence
    -implemented data persistence
    - HighScore and Name saves between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainManager : MonoBehaviour
 {
@@ -10,18 +16,44 @@ public class MainManager : MonoBehaviour
     public int LineCount = 6;
     public Rigidbody Ball;
 
+    public Text BestScoreText;
+
+    public TextMeshProUGUI MenuBestScoreText;
+
     public Text ScoreText;
     public GameObject GameOverText;
+
+    public TextMeshProUGUI NameInputField;
     
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
 
+    public static string PlayerName = "";
+
+    public static int HighScore;
+
+    private static string HighScoreName ="";
+
+    private int SpawnCount = 1;
+
+
     
     // Start is called before the first frame update
     void Start()
     {
+        LoadName();
+        MenuBestScoreText.text = $"Best score: {HighScoreName}; Score:{HighScore}";
+    
+        
+    }
+
+    void StartNew()
+    {
+        if (PlayerName != ""){
+            BestScoreText.text = $"Best score: {HighScoreName}; Score:{HighScore}";
+        }
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -41,9 +73,15 @@ public class MainManager : MonoBehaviour
     private void Update()
     {
         if (!m_Started)
-        {
+        {   
+            if (SpawnCount != 0)
+            {
+                SpawnCount -= 1;
+                StartNew();
+            }
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                StartNew();
                 m_Started = true;
                 float randomDirection = Random.Range(-1.0f, 1.0f);
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
@@ -66,11 +104,68 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+        if (m_Points > HighScore)
+        {
+            HighScore = m_Points;
+            if (PlayerName != HighScoreName)
+            {
+                HighScoreName = PlayerName;
+            }
+            SaveName();
+        }
     }
 
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string highScoreName;
+        public int highScore;
+    }
+
+    public void SaveName() 
+    {
+        SaveData data = new SaveData();
+        data.highScoreName = HighScoreName;
+        data.highScore = HighScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            HighScoreName = data.highScoreName;
+            HighScore = data.highScore;
+        }
+    }
+
+    public void ToGame()
+    {
+        PlayerName = NameInputField.text;
+        SaveName();
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        SaveName();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
